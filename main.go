329810e40bfb6d852f293dd11da16784315b3f74
stm33 <satuma33@icloud.com>
2022-10-19 09:32:58 +0900

    [add]refs #7 create-project

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"go-todo-api/controllers"
+	"go-todo-api/middleware"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	router := gin.Default()
+	engine := gin.Default()
 
-	router.LoadHTMLGlob("")
+	engine.Use(middleware.RecordUaAndTime)
+
+	bookEngine := engine.Group("/book")
+	{
+		v1 := bookEngine.Group("/v1")
+		{
+			v1.POST("/add", controllers.BookAdd)
+			v1.GET("/list", controllers.BookList)
+			v1.PUT("/update", controllers.BookUpdate)
+			v1.DELETE("/delete", controllers.BookDelete)
+		}
+	}
+	engine.Run(":8080")
 }
